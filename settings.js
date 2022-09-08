bb5bb599f6be3c7d8a36fa0e4af4d4378b3b70e6
shaheenbal <113058879+shaheenbal@users.noreply.github.com>
2022-09-08 06:57:21 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,11 +54,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
+global.vcardowner = ['923337470194'] //ur owner number
 global.ownername = "🦄Dream Guy Xeon" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Pakistan, Rahimyar, khanpur" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
